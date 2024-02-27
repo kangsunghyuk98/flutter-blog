80537cbb2ff214d11727cfdd53ec4cab051934ee
kangseonghyeok <kangsh9820@naver.com>
2024-02-27 21:07:19 +0900

    [ENH] Drawer add - 메뉴 추가를 위한 Drawer 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_01/pages/post/home_page.dart';
 import 'package:flutter_01/pages/user/join_page.dart';
 import 'package:flutter_01/pages/user/login_page.dart';
 import 'package:get/get.dart';
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: JoinPage(),
+      home: HomePage(),
     );
   }
 }
--- lib/pages/post/home_page.dart
+++ lib/pages/post/home_page.dart
@@ -5,6 +5,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Container(
+        width: 300,
+        height: double.infinity,
+        color: Colors.blue,
+      ),
+      appBar: AppBar(),
       body: Text("Home Page"),
     );
   }
