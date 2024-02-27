b2a5296c893e377029a21cc7eabcc69ac7c51090
kangseonghyeok <kangsh9820@naver.com>
2024-02-27 21:27:41 +0900

    [ENH] Drawer TextButton Add - Drawer 메뉴 추가, 메서드화 적용

--- lib/pages/post/home_page.dart
+++ lib/pages/post/home_page.dart
@@ -1,17 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_01/size.dart';
 
 class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Container(
-        width: 300,
-        height: double.infinity,
-        color: Colors.blue,
-      ),
+      drawer: _navigation(context),
       appBar: AppBar(),
       body: Text("Home Page"),
+    );
+  }
+
+  Widget _navigation(BuildContext context) {
+    return Container(
+      width: getDrawerWidth(context),
+      height: double.infinity,
+      color: Colors.white,
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextButton(
+                  onPressed: (){},
+                  child: Text(
+                    "회원 상세",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black54),
+                  )
+              ),
+              Divider(),
+              TextButton(
+                  onPressed: (){},
+                  child: Text(
+                    "로그 아웃",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black54),
+                  )
+              ),
+              Divider(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
