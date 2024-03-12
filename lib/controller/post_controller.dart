c6b2f24e7abecee6d751576972c0d1f12c6a9233
kangseonghyeok <kangsh9820@naver.com>
2024-03-12 21:15:48 +0900

    [REF] BBS Detail, List update after BBS update - 게시글 수정 후에 리스트와 디테일 find 함수 실행

--- lib/controller/post_controller.dart
+++ lib/controller/post_controller.dart
@@ -36,6 +36,8 @@ class PostController extends GetxController{
 
   Future<void> updateById(int? id, String title, String content) async{
     await _postRepository.updateById(id, title, content);
+    findById(id);
+    findAll();
   }
 
 }
