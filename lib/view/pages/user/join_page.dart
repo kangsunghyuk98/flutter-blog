e992e84277df40952bcc09fc6bcf9ac16c21fe04
kangseonghyeok <kangsh9820@naver.com>
2024-03-24 13:33:54 +0900

    [ENH] Join Page join process add - 회원가입 화면 회원가입 처리 추가

--- lib/view/pages/user/join_page.dart
+++ lib/view/pages/user/join_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_01/controller/user_controller.dart';
 import 'package:flutter_01/utils/validator_util.dart';
+import 'package:get/get.dart';
 import 'package:get/route_manager.dart';
 import 'package:validators/validators.dart';
 
@@ -14,8 +16,11 @@ class JoinPage extends StatelessWidget {
   final _password = TextEditingController();
   final _email = TextEditingController();
 
+  UserController u = Get.find();
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -65,8 +70,13 @@ class JoinPage extends StatelessWidget {
               }
           ),
           TextButton(
-              onPressed: (){
-                Get.to(LoginPage());
+              onPressed: () async{
+                String? result = await u.join(_username.text.trim(), _password.text.trim(), _email.text.trim());
+                if(result == "000") {
+                  Get.to(LoginPage());
+                } else {
+                  print("회원가입 실패하였습니다.");
+                }
               },
               child: Text("로그인 페이지로 이동")
           ),
